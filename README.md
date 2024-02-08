6e5464ca5ef51fb12bb8031baafa6209bee855aa
Gabriel Lima <44784408+gablm@users.noreply.github.com>
2024-02-08 00:54:35 +0000

    printf + gnl + libft

---
+++ README.md
@@ -0,0 +1,22 @@
+# Rank 00 - Libft
+
+![Static Badge](https://img.shields.io/badge/Score-125%2F100-green?style=for-the-badge&logo=42&color=%233a9100)
+![Static Badge](https://img.shields.io/badge/Language-C-orange?style=for-the-badge&logo=C)
+
+> Your very first own library
+
+# Rank 01 - ft_printf
+
+![Static Badge](https://img.shields.io/badge/Score-100%2F100-green?style=for-the-badge&logo=42&color=%233a9100)
+![Static Badge](https://img.shields.io/badge/Language-C-orange?style=for-the-badge&logo=C)
+
+> Because ft_putnbr() and ft_putstr() aren’t enough
+
+# Rank 01 - Get Next Line
+
+![Static Badge](https://img.shields.io/badge/Score-125%2F100-green?style=for-the-badge&logo=42&color=%233a9100)
+![Static Badge](https://img.shields.io/badge/Language-C-orange?style=for-the-badge&logo=C)
+
+> Reading a line from a fd is way too tedious
+
+These projects are part of ranks 0 and 1 of the [42](https://www.42network.org) Common Core curriculum.
---
+++ get_next_line/get_next_line/get_next_line_utils.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gfragoso <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/26 22:19:17 by gfragoso          #+#    #+#             */
+/*   Updated: 2023/08/31 18:33:29 by gfragoso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*strdup_tilln(char *str)
+{
+	int		i;
+	char	*res;
+
+	i = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (str[i] == '\n')
+		i++;
+	res = (char *)malloc(sizeof(char) * i + 1);
+	res[i] = 0;
+	while (i-- > 0)
+		res[i] = str[i];
+	return (res);
+}
+
+void	strcpy_fromn(char *str)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (str[i] == '\n')
+		i++;
+	j = 0;
+	while (str[i])
+		str[j++] = str[i++];
+	while (str[j])
+		str[j++] = 0;
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	if (!s)
+		return (0);
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (unsigned char)c)
+			return ((char *)s + i);
+		i++;
+	}
+	if ((unsigned char)c == '\0')
+		return ((char *)s + i);
+	return (0);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*res;
+	int		i;
+	int		h;
+	int		s1len;
+	int		s2len;
+
+	s1len = ft_strlen(s1);
+	s2len = ft_strlen(s2);
+	res = (char *)malloc(sizeof(char) * (s1len + s2len + 1));
+	if (!res)
+		return (NULL);
+	i = 0;
+	h = 0;
+	while (h < s1len)
+		res[i++] = s1[h++];
+	h = 0;
+	while (h < s2len)
+		res[i++] = s2[h++];
+	res[i] = 0;
+	free(s2);
+	free(s1);
+	return (res);
+}
